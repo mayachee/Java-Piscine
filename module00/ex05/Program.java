db3235450fc344921f9f1052a0fcdde37b655c8c
siiine-764 <yassinepro764@gmail.com>
2024-01-21 15:20:10 +0100

    ex05 module00

---
+++ module00/ex05/Program.java
@@ -0,0 +1,26 @@
+package module00.ex05;
+
+import java.util.Scanner;
+
+public class Program {
+    
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Hello World!");
+
+            String input = scanner.nextLine();
+
+            // Close the scanner
+            scanner.close();
+            
+            int i = 0;
+            System.out.println(input.length());
+
+            while (i < input.length()) {
+                System.out.println("input[" + i + "] = " + input.charAt(i));
+                i++;
+            }
+
+        }
+}

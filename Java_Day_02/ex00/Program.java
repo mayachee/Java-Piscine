c60e4d032c5b857dfcdfcfe13b4870287ef16c99
Knox-764 <80540449+siiine-764@users.noreply.github.com>
2024-01-26 21:50:19 +0000

    beginn day02

---
+++ Java_Day_02/ex00/Program.java
@@ -0,0 +1,7 @@
+package Java_Day_02.ex00;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# java-module
+# Java Piscine

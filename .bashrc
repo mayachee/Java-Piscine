35f055c5c47b01ba767438e11d36478e0d0125dc
mayachee <80540449+mayachee@users.noreply.github.com>
2024-12-24 12:37:20 +0000

    Execute command: echo 'Hello, world!'

---
+++ .bashrc
@@ -0,0 +1,1 @@
+alias pushi="./execute_command.sh"
---
+++ create_alias.sh
@@ -0,0 +1,6 @@
+# Create an alias named 'pushi' to execute the script
+
+alias pushi="./execute_command.sh"
+
+# Print a message to inform the user
+echo "Alias 'pushi' created to execute './execute_command.sh'"
